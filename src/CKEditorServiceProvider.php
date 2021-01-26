6d4864aa4c6bbbb33c08f45b80bee8dcace7a0a8
zhoutao <zt7515@gmail.com>
2021-01-26 16:31:05 +0800

    create ckeditor5

---
+++ src/CKEditorServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ghost\CKEditor;
+
+use Encore\Admin\Admin;
+use Encore\Admin\Form;
+use Illuminate\Support\ServiceProvider;
+
+class CKEditorServiceProvider extends ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot(CKEditor $extension)
+    {
+        if (! CKEditor::boot()) {
+            return ;
+        }
+
+        if ($views = $extension->views()) {
+            $this->loadViewsFrom($views, 'ghost-ckeditor');
+        }
+
+        if ($this->app->runningInConsole() && $assets = $extension->assets()) {
+            $this->publishes(
+                [$assets => public_path('vendor/ghost/ckeditor')],
+                'ghost-ckeditor'
+            );
+        }
+        
+	    Admin::booting(function () {
+		    Form::extend('editor', Editor::class);
+		    $config = (array) CKEditor::config('config');
+		    if (empty($config['language'])) $config['language']=strtolower((string) config('app.locale'));
+		    Admin::js('vendor/ghost/ckeditor/translations/'.$config['language'].'.js');
+	    });
+    }
+}
